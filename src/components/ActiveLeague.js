01dc72e0c047700f38d3bdbf64400e89fd92011c
Jason Roundtree <jasontree123@gmail.com>
2018-06-01 16:28:10 -0700

    Add fields that should display for a given league

--- src/components/ActiveLeague.js
+++ src/components/ActiveLeague.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import PointsTable from './PointsTable';
+import Standings from './Standings';
 import { Link } from 'react-router-dom';
 
-const ActiveLeagues = () => {
+const ActiveLeague = () => {
     return (
         <div>
-            <h2>Active Leagues</h2>
-            <button><Link to="/dashboard/record-round">Record New Round</Link></button>
+            <h2>{'League Name'}</h2>
+            <button><Link to="/dashboard/active-leagues/:id/create-round">Create New Round</Link></button>
+            <h3>Completed Rounds:</h3>
+            <h3>Players:</h3>
+            <h3>League Settings:</h3>
+            <PointsTable />
+            <h3>Standings:</h3>
+            <Standings />
         </div>
         
     );
 }
 
-export default ActiveLeagues;
+export default ActiveLeague;
