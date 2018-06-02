c62d4d33486780091faef329350d8a349bdfdbda
Jason Roundtree <jasontree123@gmail.com>
2018-06-01 19:27:41 -0700

    Add Footer component

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navigation from './Navigation';
 import Router from './Router';
+import Footer from './Footer';
 import '../index.css';
 
 const App = () => {
     return (
         <BrowserRouter>
             <div>
-                <h1>Clash for the Cup</h1>
-                <Navigation />
-                <Router />
+                <div id="content">
+                    <h1>Clash for the Cup</h1>
+                    <Navigation />
+                    <Router />
+                </div>
+                <Footer />
             </div>
         </BrowserRouter>
     );
