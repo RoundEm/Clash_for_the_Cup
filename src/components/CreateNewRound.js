1898e1332754f7daeaf4e098d0b3eb48f90a0a07
Jason Roundtree <jasontree123@gmail.com>
2018-06-01 18:48:57 -0700

    Add Players component

--- src/components/CreateNewRound.js
+++ src/components/CreateNewRound.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Players from './Players';
 
+// TODO: AJAX request to get a list of players in league
 const CreateNewRound = () => {
   return (
     <div>
@@ -13,6 +15,7 @@ const CreateNewRound = () => {
                 <label>Event Name</label>
                 <input type="text"></input>
                 <label>Players</label>
+                <Players />
             </fieldset>
         </form>
     </div>
