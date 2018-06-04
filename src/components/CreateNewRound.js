026867eae456de24fcecd976ab73a5fa9fb0bfa1
Jason Roundtree <jasontree123@gmail.com>
2018-06-04 15:24:31 -0700

    Fix input syntax and remove Players: label

--- src/components/CreateNewRound.js
+++ src/components/CreateNewRound.js
@@ -2,24 +2,25 @@ import React from 'react';
 import Players from './Players';
 
 // TODO: AJAX request to get a list of players in league
-const CreateNewRound = () => {
-  return (
-    <div>
-        <h2>Create New Round</h2>
-        <form>
-            <fieldset>
-                <label>Date</label>
-                <input type="date"></input>
-                <label>Course Name</label>
-                <input type="text"></input>
-                <label>Event Name</label>
-                <input type="text"></input>
-                <label>Players</label>
-                <Players />
-            </fieldset>
-        </form>
-    </div>
-  );
+class CreateNewRound extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Create New Round</h2>
+                <form>
+                    <fieldset>
+                        <label htmlFor="round-date">Date</label>
+                        <input type="date" id="round-date" />
+                        <label htmlFor="round-course">Course Name</label>
+                        <input type="text" id="round-course" />
+                        <label htmlFor="round-name">Event Name</label>
+                        <input type="text" id="round-name" />
+                        <Players />
+                    </fieldset>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default CreateNewRound;
