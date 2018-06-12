adf0e5091a3bdb1b2aa99692f91fc04412c3d16c
Jason Roundtree <jasontree123@gmail.com>
2018-06-11 17:52:04 -0700

    Remove some unused components and data

--- src/components/ActiveLeague.js
+++ src/components/ActiveLeague.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import Players from './Players';
 import CompletedRounds from './CompletedRounds';
-import PointsTable from './PointsTable';
+import PointsSettingsTable from './PointsSettingsTable';
 import Standings from './Standings';
 import { Link } from 'react-router-dom';
 
 // TODO: AJAX request to get data for: league name, points table; completed rounds; players; standings
-const ActiveLeague = () => {
-    return (
-        <div>
-            <h2>{'League Name'}</h2>
-            <button><Link to="/dashboard/active-leagues/:id/create-round">Create New Round</Link></button>
-            <h3>Completed Rounds:</h3>
-            <CompletedRounds />
-            <h3>Players:</h3>
-            <Players />
-            <h3>Standings:</h3>
-            <Standings />
-            <h3>League Settings:</h3>
-            <PointsTable />
-        </div>
-        
-    );
+class ActiveLeague extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>{'League Name'}</h2>
+                <button><Link to="/dashboard/active-leagues/:id/create-round">Create New Round</Link></button>
+                <h3>Players:</h3>
+                <Players />
+                <h3>Completed Rounds:</h3>
+                <CompletedRounds />
+                <h3>Standings:</h3>
+                <Standings />
+                <h3>Points Settings:</h3>
+                
+            </div>
+            
+        );
+    }
 }
 
 export default ActiveLeague;
