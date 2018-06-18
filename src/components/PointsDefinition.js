9b821b385ac6194b61bcad854381e1801f354b48
Jason Roundtree <jasontree123@gmail.com>
2018-06-18 12:09:51 -0700

    Remove unused code and add styling and rendering of points definitions

--- src/components/PointsDefinition.js
+++ src/components/PointsDefinition.js
@@ -1,56 +1,30 @@
 import React from 'react';
-import { clone } from 'lodash';
 import EditableField from './EditableField';
+import styled from 'styled-components';
 
-class PointsDefinition extends React.Component {
-    constructor(props) {
-        super(props);
+const PointDefRow = styled.div`
+    width: 40%;
+    display: flex;
+    /* justify-content: space-between; */
+    border-bottom: 1px solid rgb(163, 248, 230);
+    padding: 5px 0 0;
+    p {
+        display: inline-block;
+        margin: 0 35px 15px 0;
     }
-    // assignDataToState = () => (
-    //     console.log('assignDataToState ran'),
-    //     this.setState({
-    //         pointsData: [...this.props.pointTypes]
-    //     });
-    // )
-    // renderPointsSettings = () => this.state.pointsData.map((point, i) => (
-    //     <tr key={point.type + i}>
-    //         <td>{point.type}</td>
-    //         <td className="point-cell">
-    //             <EditableField 
-    //                 value={point.weight || 0} 
-    //                 onUpdate={text => this.handleUpdate(text, i)} 
-    //             />
-    //         </td>
-    //     </tr>
-    // ));
-    // handleUpdate = (text, i) => {
-    //     const cachedSetting = clone(this.state.pointsData[i]);
-    //     const newPoints = Object.assign({...this.state.pointsData[i]}, { points: text || cachedSetting.points });
-    //     this.setState({
-    //         pointsData: [...this.state.pointsData.slice(0, i), newPoints, ...this.state.pointsData.slice(i + 1)]
-    //     });
+`
 
-    // }
-    // componentDidUpdate() {
-    //     console.log('this.state.pointsData: ', this.state.pointsData)
-    // }
-
-    
-    render() {
-        return (
-            <div>
-                <input 
-                    type="text" 
-                    value={this.props.type}
-                    onUpdate={e => this.props.onUpdate(e.target.value, this.props.weight)}
-                />
+const PointsDefinition = props => {
+    return (
+        <PointDefRow>
+                <p>{props.type}:</p>
                 <EditableField 
-                    value={this.props.weight} 
-                    onUpdate={text => this.props.onUpdate(this.props.type, text)}
+                    value={props.weight} 
+                    onUpdate={text => props.onUpdate(props.type, text)}
                 />
-            </div>
-        );
-    }
+        </PointDefRow>
+    );
+
 }
 
 export default PointsDefinition;
