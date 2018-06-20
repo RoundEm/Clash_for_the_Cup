f78ba45e83d65962bd13f50bec19ac3eb5ffbe9c
Jason Roundtree <jasontree123@gmail.com>
2018-06-19 21:00:18 -0700

    Add styling and move history.back fn to button

--- src/components/CreateLeague.js
+++ src/components/CreateLeague.js
@@ -3,25 +3,30 @@ import BasicSettings from './BasicSettings';
 import PointSettings from './PointSettings';
 import PlayerSettings from './PlayerSettings';
 import styled from 'styled-components';
-const axios = require('axios');
-const { API_BASE_URL } = require('../config');
 
 const Style = styled.div`
     width: 650px;
     margin: 7px auto;
     padding: 10px;
-    border: 1px solid black;
+    border: 2px solid #b8eac8;
     border-radius: 5px;
-    background-color: #E6E6FA;
+    background-color: #D3D3D3;
     overflow: hidden;
     h2 {
         margin: 5px 0 15px;
+        color: #24b68e;
+    }
+    li, p, label {
+        color: grey;
+    }
+    h3 {
+        color: grey;
     }
     label {
         display: block;
     }
     input {
-        border: 1px solid #E6E6FA;
+        border: 1px solid #D3D3D3;
     }
     button.save-settings {
         margin: 15px 0 5px;
@@ -29,7 +34,7 @@ const Style = styled.div`
     }
     div.section-container {
         background-color: white;
-        border: 1px solid #E6E6FA;
+        border: 1px solid #D3D3D3;
         border-radius: 5px;
         margin: 5px;
         padding: 10px;
@@ -51,9 +56,6 @@ class CreateLeague extends React.Component {
     }
     handlePlayerSave = () => {
         this.refs.childPlayer.updateActiveComponent(1);
-    }
-    handleDone = () => {
-        window.history.back();
     }
     componentDidUpdate() {
         console.log('CreateLeague activeComponent: ', this.state.activeComponent)
@@ -77,6 +79,7 @@ class CreateLeague extends React.Component {
                         leagueId={this.state.leagueId} 
                         ref="childPlayer"
                     />
+                    <button onClick={() => window.history.back()}>Done</button>
                 </div>
             </Style>
         );
