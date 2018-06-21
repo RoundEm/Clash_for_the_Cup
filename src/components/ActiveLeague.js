c9d9a0f85d6841379f3986ee2edb020cedfd0b70
Jason Roundtree <jasontree123@gmail.com>
2018-06-21 15:43:01 -0700

    Add fn that calculates total per player

--- src/components/ActiveLeague.js
+++ src/components/ActiveLeague.js
@@ -21,7 +21,7 @@ class ActiveLeague extends React.Component {
         // GET league players
         axios.get(`${API_BASE_URL}/leagues/${this.state.leagueId}`)
             .then(res => {
-                // console.log('res.data: ', res.data)
+                console.log('league players res.data: ', res.data)
                 const players = res.data.players;
                 const leagueName = res.data.name;
                 const rounds = res.data.rounds;
@@ -36,11 +36,37 @@ class ActiveLeague extends React.Component {
             .catch(err => {
                 console.log(err);
             });
+             // GET all players total points
+                axios.get(`${API_BASE_URL}/leagues/${this.state.leagueId}/points-allocation`)
+                    .then(res => {
+                        console.log('all player points res: ', res.data)
+                        this.handlePointTotals(res.data)
+                    })
+                    .catch(err => {
+                        console.log(err);
+            });
+    }
+    handlePointTotals = data => {
+        let id = data.player;
+        let totalPoints = []
+        data.reduce((accumulator, player) => {
+            console.log(accumulator)
+            if(!accumulator[player.id]) {
+                accumulator[player.id] = {
+                    id: player.id,
+                    total: 0
+                }
+                totalPoints.push(accumulator[player.id])
+            }
+            accumulator[player.id].total += player.total
+            return accumulator
+        }, {});
+        console.log('totalPoints: ', totalPoints)
     }
     render() {
         return (
             <div className="active-league">
-                <h2>League Name: {this.state.leagueName}</h2>
+                <h2>League Name: <span style={{color: 'white', margin: '0 0 0 5px'}}>{this.state.leagueName}</span></h2>
                 <h3>Players:</h3>
                 <PlayersRender 
                     players={this.state.players}
@@ -52,7 +78,10 @@ class ActiveLeague extends React.Component {
                     league={this.state.leagueId}
                 />
                 <h3>Standings:</h3>
-                <Standings />
+                <Standings 
+                    players={this.state.players}
+                    points={this.state.pointTotals}
+                />
                 <h3>Points Settings:</h3>
                 {/* TODO: Make this a component and also use it on ViewRound component */}
                 <table>
@@ -65,7 +94,7 @@ class ActiveLeague extends React.Component {
                         ))}
                     </tbody>
                 </table>
-                
+                <button>Done</button>
             </div>
             
         );
