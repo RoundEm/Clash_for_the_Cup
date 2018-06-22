561cc60490f94a4c896a4a561fe07478d265159f
Jason Roundtree <jasontree123@gmail.com>
2018-06-22 15:45:31 -0700

    Show player names instead of ids

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -4,19 +4,21 @@ const { API_BASE_URL } = require('../config');
 
 const Standings = props => {
   console.log('Standings props: ', props)
+  // const playersWithPoints = props.players;
   return (
       <table>
         <tbody>
           <tr>
-            {/* <th>Place</th> */}
             <th>Player</th>
             <th>Points</th>
           </tr>
-          {props.points.map(player => (
-            <tr>
-              <td>{player.id}</td>
-              <td>{player.total}</td>
-            </tr>
+          {/* TODO: Refactor these loops (along with CreateRound & ActiveLeague)  so that you don't have to do so much looping to get name?? */}
+          {props.points.map((player, i) => (
+              <tr key={player + i}>
+                <td>{props.players.map((_player) => (
+                  _player._id === player.id ? _player.name : ''))}</td>
+                <td>{player.total}</td>
+              </tr>
           ))}
         </tbody>
       </table>
