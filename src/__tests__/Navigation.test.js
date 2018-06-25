1c69c36e36150581b9d55e1c23d3db0e8b1d1823
Jason Roundtree <jasontree123@gmail.com>
2018-06-25 16:54:01 -0700

    push setupTests.js and smoke test for Navigation

---
+++ src/__tests__/Navigation.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import Navigation from '../components/Navigation';
+
+describe('<Navigation />', () => {
+    it('Renders without crashing', () => {
+        shallow(<Navigation />);
+    });
+    
+});
